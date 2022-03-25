5ead28739770f984fbd2fa491ac3d42355cf6a02
Trang Luong <luongtrang014@gmail.com>
2022-03-25 17:15:53 +0800

    add to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState, useEffect } from "react";
+import Avatar from "./components/Avatar";
+import { PartList } from "./components/PartList";
+const total = {
+  body: 17,
+  eyes: 17,
+  hair: 73,
+  mouth: 24,
+  eyebrows: 15,
+  glasses: 17,
+  clothing1: 5,
+  clothing2: 5,
+  clothing3: 9,
+};
 
 function App() {
+  const [body, setBody] = useState();
+  const [eyes, setEyes] = useState();
+  const [hair, setHair] = useState();
+  const [mounth, setMounth] = useState();
+  const [eyebrows, setEyebrows] = useState();
+  const [glasses, setGlasses] = useState();
+  const [clothing1, setClothing1] = useState();
+  const [clothing2, setClothing2] = useState();
+  const [clothing3, setClothing3] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
