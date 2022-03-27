c33a565e9cf2d7852c5f7e47985beddce20b601d
Trang Luong <luongtrang014@gmail.com>
2022-03-27 15:49:43 +0800

    update random function

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   const [body, setBody] = useState(1);
   const [eyes, setEyes] = useState(1);
   const [hair, setHair] = useState(1);
-  const [mounth, setMouth] = useState(1);
+  const [mouth, setMouth] = useState(1);
   const [eyebrows, setEyebrows] = useState(1);
   const [facialHair, setFacialHair] = useState(0);
   const [earrings, setEarrings] = useState(0);
@@ -32,23 +32,39 @@ function App() {
   const [clothing2, setClothing2] = useState(1);
   const [clothing3, setClothing3] = useState(1);
   const randomize = () => {
-    setBody(Math.floor(Math.random() * total.body));
-    setEyes(Math.floor(Math.random() * total.eyes));
-    setHair(Math.floor(Math.random() * total.hair));
-    setMouth(Math.floor(Math.random() * total.mouth));
-    setEyebrows(Math.floor(Math.random() * total.eyebrows));
-    setFacialHair(Math.floor(Math.random() * total.facial_hair));
-    setEarrings(Math.floor(Math.random() * total.earrings));
-    setHat(Math.floor(Math.random() * total.hat));
-    setGlasses(Math.floor(Math.random() * total.glasses));
-    setNeckWear(Math.floor(Math.random() * total.neckwear));
-    setClothing1(Math.floor(Math.random() * total.clothing1));
-    setClothing2(Math.floor(Math.random() * total.clothing2));
-    setClothing3(Math.floor(Math.random() * total.clothing3));
+    setBody(Math.floor(Math.random() * total.body) + 1);
+    setEyes(Math.floor(Math.random() * total.eyes) + 1);
+    setHair(Math.floor(Math.random() * total.hair) + 1);
+    setMouth(Math.floor(Math.random() * total.mouth) + 1);
+    setEyebrows(Math.floor(Math.random() * total.eyebrows) + 1);
+    setFacialHair(Math.floor(Math.random() * total.facial_hair) + 1);
+    setEarrings(Math.floor(Math.random() * total.earrings) + 1);
+    setHat(Math.floor(Math.random() * total.hat) + 1);
+    setGlasses(Math.floor(Math.random() * total.glasses) + 1);
+    setNeckWear(Math.floor(Math.random() * total.neckwear) + 1);
+    setClothing1(Math.floor(Math.random() * total.clothing1) + 1);
+    setClothing2(Math.floor(Math.random() * total.clothing2) + 1);
+    setClothing3(Math.floor(Math.random() * total.clothing3) + 1);
   };
+
   useEffect(() => {
     randomize();
   }, []);
+
+  const [intervalId, setIntervalId] = useState(0);
+
+  const handleRandom = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      setIntervalId(0);
+      return;
+    }
+
+    const newIntervalId = setInterval(() => {
+      randomize();
+    }, 300);
+    setIntervalId(newIntervalId);
+  };
 
   const getImgSource = (part, n) => {
     const imgSource = `./character/${part}/${n}.png`;
@@ -115,7 +131,7 @@ function App() {
             ></img>
 
             <img
-              src={process.env.PUBLIC_URL + getImgSource("mouths", mounth)}
+              src={process.env.PUBLIC_URL + getImgSource("mouths", mouth)}
               alt=""
               width="250"
               style={{ position: "absolute" }}
@@ -173,7 +189,7 @@ function App() {
             ></img>
           </div>
           <div className="random">
-            <button className="button-random" onClick={randomize}>
+            <button className="button-random" onClick={handleRandom}>
               Random
             </button>
           </div>
@@ -184,7 +200,10 @@ function App() {
             <div className="list-title"> Body </div>
             <div className="list">
               {getArrayPart(17).map((item) => (
-                <button onClick={() => setBody(item)} className="image">
+                <button
+                  onClick={() => setBody(item)}
+                  className={body === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={process.env.PUBLIC_URL + getImgSource("body", item)}
                     alt=""
@@ -199,7 +218,10 @@ function App() {
             <div className="list-title"> Eyes </div>
             <div className="list">
               {getArrayPart(17).map((item) => (
-                <button onClick={() => setEyes(item)} className="image">
+                <button
+                  onClick={() => setEyes(item)}
+                  className={eyes === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={process.env.PUBLIC_URL + getImgSource("eyes", item)}
                     alt=""
@@ -214,7 +236,10 @@ function App() {
             <div className="list-title"> Hair </div>
             <div className="list">
               {getArrayPart(73).map((item) => (
-                <button onClick={() => setHair(item)} className="image">
+                <button
+                  onClick={() => setHair(item)}
+                  className={hair === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={process.env.PUBLIC_URL + getImgSource("hair", item)}
                     alt=""
@@ -229,7 +254,10 @@ function App() {
             <div className="list-title"> Clothing Layer 1 </div>
             <div className="list">
               {getArrayPart(5).map((item) => (
-                <button onClick={() => setClothing1(item)} className="image">
+                <button
+                  onClick={() => setClothing1(item)}
+                  className={clothing1 === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={process.env.PUBLIC_URL + getImgSource("layer_1", item)}
                     alt=""
@@ -244,7 +272,10 @@ function App() {
             <div className="list-title"> Clothing layer 2 </div>
             <div className="list">
               {getArrayPart(5).map((item) => (
-                <button onClick={() => setClothing2(item)} className="image">
+                <button
+                  onClick={() => setClothing2(item)}
+                  className={clothing2 === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={process.env.PUBLIC_URL + getImgSource("layer_2", item)}
                     alt=""
@@ -259,7 +290,10 @@ function App() {
             <div className="list-title"> Clothing layer 3 </div>
             <div className="list">
               {getArrayPart(9).map((item) => (
-                <button onClick={() => setClothing3(item)} className="image">
+                <button
+                  onClick={() => setClothing3(item)}
+                  className={clothing3 === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={process.env.PUBLIC_URL + getImgSource("layer_3", item)}
                     alt=""
@@ -274,7 +308,10 @@ function App() {
             <div className="list-title"> Mouth </div>
             <div className="list">
               {getArrayPart(24).map((item) => (
-                <button onClick={() => setMouth(item)} className="image">
+                <button
+                  onClick={() => setMouth(item)}
+                  className={mouth === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={process.env.PUBLIC_URL + getImgSource("mouths", item)}
                     alt=""
@@ -289,7 +326,10 @@ function App() {
             <div className="list-title"> Facial hair </div>
             <div className="list">
               {getArrayPart(18).map((item) => (
-                <button onClick={() => setFacialHair(item)} className="image">
+                <button
+                  onClick={() => setFacialHair(item)}
+                  className={facialHair === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={
                       process.env.PUBLIC_URL + getImgSource("facial_hair", item)
@@ -306,7 +346,10 @@ function App() {
             <div className="list-title"> Eyebrows </div>
             <div className="list">
               {getArrayPart(15).map((item) => (
-                <button onClick={() => setEyebrows(item)} className="image">
+                <button
+                  onClick={() => setEyebrows(item)}
+                  className={eyebrows === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={
                       process.env.PUBLIC_URL + getImgSource("eyebrows", item)
@@ -323,7 +366,10 @@ function App() {
             <div className="list-title"> Glasses </div>
             <div className="list">
               {getArrayPart(18).map((item) => (
-                <button onClick={() => setGlasses(item)} className="image">
+                <button
+                  onClick={() => setGlasses(item)}
+                  className={glasses === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={process.env.PUBLIC_URL + getImgSource("glasses", item)}
                     alt=""
@@ -338,7 +384,10 @@ function App() {
             <div className="list-title"> Earrings </div>
             <div className="list">
               {getArrayPart(33).map((item) => (
-                <button onClick={() => setEarrings(item)} className="image">
+                <button
+                  onClick={() => setEarrings(item)}
+                  className={earrings === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={
                       process.env.PUBLIC_URL + getImgSource("earrings", item)
@@ -355,7 +404,10 @@ function App() {
             <div className="list-title"> Hat </div>
             <div className="list">
               {getArrayPart(29).map((item) => (
-                <button onClick={() => setHat(item)} className="image">
+                <button
+                  onClick={() => setHat(item)}
+                  className={hat === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={process.env.PUBLIC_URL + getImgSource("hats", item)}
                     alt=""
@@ -370,7 +422,10 @@ function App() {
             <div className="list-title"> Neckwear </div>
             <div className="list">
               {getArrayPart(19).map((item) => (
-                <button onClick={() => setNeckWear(item)} className="image">
+                <button
+                  onClick={() => setNeckWear(item)}
+                  className={neckWear === item ? "choosen-img" : "image"}
+                >
                   <img
                     src={
                       process.env.PUBLIC_URL + getImgSource("neckwear", item)
